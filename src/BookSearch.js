96e5ca0bd30fbaca83ebb4940fd3489f240f351a
Joseph McMurray <joemoe1984@gmail.com>
2017-08-17 07:32:15 -0700

    Added: better Unique keys

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import BooksComponent from './BooksComponent'
 import BooksList from './BooksList'
-// import * as BooksAPI from './BooksAPI'
 
 let query = '';
 
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,20 +1,27 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import shortid from 'shortid'
 
 class BooksList extends Component {
+	static id = ''
+
 	static propTypes = {
 		books: PropTypes.array.isRequired,
 		moveBook: PropTypes.func.isRequired,
 		labels: PropTypes.array.isRequired,
 	}
-	render() {
 
+	componentDidMount() {
+		this.id = shortid.generate();
+	}
+
+	render() {
 		return (
 			<ol className="books-grid">
 				{ this.props.books.length > 0 && (
 
 					this.props.books.map((book) => (
-						<li key={book.id}>
+						<li key={this.id + '-' + book.id}>
 							<div className="book">
 								<div className="book-top">
 									<div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
@@ -25,8 +32,7 @@ class BooksList extends Component {
 										}}>
 											<option value="none" disabled>Move to...</option>
 											{this.props.labels.map(shelf => (
-
-												<option key={shelf.slug} value={shelf.slug}>{shelf.label}</option>
+												<option key={shortid.generate()} value={shelf.slug}>{shelf.label}</option>
 											))}
 											<option value="none">None</option>
 										</select>
@@ -35,7 +41,7 @@ class BooksList extends Component {
 								<div className="book-title">{book.title}</div>
 								{typeof book.authors !== 'undefined' && (
 									<div className="book-authors">{book.authors.map((author) => (
-										<div key={book.id + "-" + author}>{author}</div>
+										<div key={shortid.generate()}>{author}</div>
 										)
 									)}</div>
 								)}
